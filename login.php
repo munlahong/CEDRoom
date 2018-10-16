f1ff9db358ad1fd749f5bc644a2851503f33c076
Pratshaya <s5802041620131@email.kmutnb.ac.th>
2018-10-16 15:23:29 +0700

    change html to php

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<html xmls="http://www.w3.org/1999/xhtml">
+<?php<html xmls="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title> aaaaaaaaaaaaa</title>
@@ -27,6 +27,6 @@
 	
 </table>
 </body>
-</html>
+</html>?>
 
 				
